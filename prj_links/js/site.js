921fbd6576fdf46688f421304fba2623c905708a
satfaat <satfaat@gmail.com>
2019-08-23 15:15:46 +0400

    wrk

--- prj_links/js/site.js
+++ prj_links/js/site.js
@@ -8,7 +8,7 @@ var meta = [
     '<meta name="viewport" content="width=device-width, initial-scale=1">'];
 var linkcss = [
     "<link rel='stylesheet' href='https://www.w3schools.com/w3css/4/w3.css'>",
-    "<link rel='stylesheet' href='css/menuNlinks.css'/>"];
+    "<link rel='stylesheet' href='css/site.css'/>"];
 var linkjs = [    
     '<script src="js/site.js"></script>'];
 var linkjsLib = ['<script src="https://www.w3schools.com/lib/w3.js"></script>',
