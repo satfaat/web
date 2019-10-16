9890c391d0f3dd31378af3781c4f4a4a2a1363b2
satfaat <satfaat@gmail.com>
2019-10-16 08:08:19 +0400

    wrk

--- prj_lnk/js/scr.js
+++ prj_lnk/js/scr.js
@@ -1,4 +1,4 @@
-'use strict'
+/*'use strict'*/
 
 /* INCLUDE */ 
 var title = (document.body.id !="") ? title = document.body.id + " " + navigator.onLine : title = "No title ";
@@ -8,7 +8,7 @@ var meta = [
     '<meta name="viewport" content="width=device-width, initial-scale=1.0">'];
 var linkcss = [
     "<link rel='stylesheet' href='https://www.w3schools.com/w3css/4/w3.css'>",
-    "<link rel='stylesheet' href='css/main.css'/>"];
+    "<link rel='stylesheet' href='css/style.css'/>"];
 var linkjs = [    
     '<script src="js/site.js"></script>'];
 var linkjsLib = ['<script src="https://www.w3schools.com/lib/w3.js"></script>',
@@ -38,3 +38,29 @@ if (window.File && window.FileReader && window.FileList && window.Blob) {
     } else {
         alert('The File APIs are not fully supported in this browser.');
     }
+
+/*** tab links ***/
+function open_it(evt, it_name) {
+    let i, tab_content, tab_links;
+
+    tab_content = document.getElementsByClassName("tab-content");
+    console.log(tab_content);
+    /*for (item in tab_content){
+        tab_content[item].style.display = "none";
+    }*/
+    for (i = 0; i < tab_content.length; i++) {
+        tab_content[i].style.display = "none";
+    }
+
+    tab_links = document.getElementsByClassName("tab-lnks");
+    console.log(tab_links);
+    /*for (item in tab_links) {
+        tab_links[item].className.replace(" active", "");
+    }*/
+    for (i = 0; i < tab_links.length; i++) {
+        tab_links[i].className.replace(" active", "");
+    }
+
+    document.getElementById(it_name).style.display = "grid";
+    evt.currentTarget.className += " active";
+}
