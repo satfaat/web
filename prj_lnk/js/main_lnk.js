aa46f466703190650575326e925909145f7790f2
satfaat <satfaat@gmail.com>
2019-10-19 15:16:18 +0400

    hm

--- prj_lnk/js/main_lnk.js
+++ prj_lnk/js/main_lnk.js
@@ -8,7 +8,7 @@ var meta = [
     '<meta name="viewport" content="width=device-width, initial-scale=1.0">'];
 var linkcss = [
     "<link rel='stylesheet' href='https://www.w3schools.com/w3css/4/w3.css'>",
-    "<link rel='stylesheet' href='css/main.css'/>"];
+    "<link rel='stylesheet' href='css/style.css'/>"];
 var linkjs = [    
     '<script src="js/site.js"></script>'];
 var linkjsLib = ['<script src="https://www.w3schools.com/lib/w3.js"></script>',
