bcbd542c40b7c1a5abafd2108722de1a6685db47
satfaat <satfaat@gmail.com>
2021-07-04 23:27:22 +0400

    update

--- json-data-retrive/run.js
+++ json-data-retrive/run.js
@@ -5,7 +5,12 @@ obj.employees[1].firstName + " " + obj.employees[1].lastName;
 document.getElementById("json-dt").innerHTML =
 lnks.first + " " + lnks.second;
 
-document.getElementsByClassName("code_web_tools")[0].href = my_links[0];
+//document.getElementsByClassName("code_web_tools")[0].href = my_links[0];
 //console.log(my_links[0]);
 //document.getElementById("lnk").href = my_links[0];
 //document.getElementsByClassName("code_web_tools").length
+
+let code_web_tools = document.getElementsByClassName("code_web_tools")
+for (let el in code_web_tools){
+    code_web_tools[el].href = my_links[el];
+}
