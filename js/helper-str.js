a32d8148459c71d7200fd3695989f4ed540342f7
satfaat <satfaat@gmail.com>
2021-08-14 13:45:03 +0400

    update

---
+++ js/helper-str.js
@@ -0,0 +1,10 @@
+hello = (val) => "Hello " + val;
+const get_char_length = (phrase) => phrase.length;
+func_name = function(phrase){return phrase.length;}
+
+
+const helper_str = {
+    "get_size": (phrase) => phrase.length,
+
+}
+//helper_str.get_char_length3('how much')
