0edc048a9a16a52ba4c79688ad76d497656cac4a
satfaat <satfaat@gmail.com>
2023-04-01 23:19:50 +0500

    up

---
+++ srv.deno/demo-http-server.js
@@ -0,0 +1,7 @@
+import { serve } from
+"https://deno.land/std@0.84.0/http/server.ts";
+
+
+for await (const req of serve(":8080")) {
+req.respond({ body: "deno server" });
+}
---
+++ srv.deno/demodeno.js
@@ -0,0 +1,3 @@
+import { serve } from 'https://deno.land/std@0.83.0/http/server.ts'
+
+console.log('Output from deno');
